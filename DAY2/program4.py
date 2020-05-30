dc8c9e174551087804970becdd24d5951c234791
Raveet kumar <66063741+ravi7799@users.noreply.github.com>
2020-05-30 23:34:00 +0530

    Update program4.py

--- DAY2/program4.py
+++ DAY2/program4.py
@@ -1,11 +1,12 @@
 #program number 4
 #To print a pattern
 
-for i in range(0,10):
-    for j in range(0,10):
-        if i+j<=4 or j-i>=5:
+n=int(input("enter the value in N:"))
+for i in range(0,n):
+    for j in range(0,n):
+        if i+j<=n/2-1 or j-i>=n/2:
             print("*",end=" ")
-        elif i-j>=5 or i+j>=14:
+        elif i-j>=n/2 or i+j>=n+n/2-1:
             print("*",end=" ")
         else:
             print(" ",end=" ")
