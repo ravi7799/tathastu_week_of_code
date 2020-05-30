579f9d13559300494c5ff1bf1d51930f93175074
Raveet kumar <66063741+ravi7799@users.noreply.github.com>
2020-05-30 23:36:01 +0530

    Update program3.py

--- DAY2/program3.py
+++ DAY2/program3.py
@@ -1,10 +1,13 @@
 #program number 3
 #print the given pattern
 
-for i in range(0,8):
-    for j in range(0,8):
-        if i==j or i+j==7:
+
+n=int(input("enter the value in N:"))
+for i in range(0,n):
+    for j in range(0,n):
+        if i==j or i+j==n-1:
             print("*",end="")
         else:
             print(" ",end="")
     print()
+
