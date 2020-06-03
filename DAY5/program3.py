09ee8f140a1d04b2a1e9bd060932eaddc352cf99
Raveet kumar <66063741+ravi7799@users.noreply.github.com>
2020-06-03 23:23:49 +0530

    Update program3.py

--- DAY5/program3.py
+++ DAY5/program3.py
@@ -1,22 +1,32 @@
-#assuming value of house cannot be negative
-def max_stole(list1,len1):
-    if len1==0:
-        return 0
-    elif len1==1:
-        return list1[0]
-    elif len1==2:
-        return max(list1[0],list1[1])
-    max_val=None
-    val1=list1[0]
-    val2=max(list1[0],list1[1])
-    for i in range(2,n):
-        max_val=max(val1+list1[i],val2)
-        val1 = val2
-        val2 = max_val
-    return max_val
+def maxvalue(list1,k):
+    sum=0
+    cursum=0
+    if k%2==0:
+        for i in range(k,len(list1),2):
+            if cursum+list1[i]>sum:
+                sum=cursum+list1[i]
+                cursum=cursum+list1[i]
+            else:
+                continue
+        return sum
+    else:
+        for i in range(k,len(list1),2):
+            if cursum+list1[i]>sum:
+                sum=cursum+list1[i]
+                cursum=cursum+list1[i]
+            else:
+                continue
+        return sum
+
 
 n=int(input("enter the number of houses in a line:"))
 lis=[]
 for i in range(n):
     lis.append(int(input("enter the value of house")))
-print(max_stole(lis,n))
+sum1=maxvalue(lis,0)
+sum2=maxvalue(lis,1)
+if sum1>sum2:
+    print(sum1)
+else:
+    print(sum2)
+
