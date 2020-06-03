d4a9238e70515e1a9aaf419033e3f0bf02560833
Raveet kumar <66063741+ravi7799@users.noreply.github.com>
2020-06-03 15:39:41 +0530

    Update program5.py

--- DAY5/program5.py
+++ DAY5/program5.py
@@ -1,15 +1,16 @@
 list1=[5,3,36,421,8,54,32,865,13,72,12]
-even=[]
-odd=[]
-print("list before:",list1)
-for i in range(len(list1)):
-    if list1[i]%2==0:
-        even.append(list1[i])
-    else:
-        odd.append(list1[i])
-even.sort(reverse=True)
-odd.sort()
-list1.clear()
-list1.extend(odd)
-list1.extend(even)
-print("list after:",list1)
+
+def evenoddsort(lis):
+    even=[]
+    odd=[]
+    for i in range(len(lis)):
+        if lis[i]%2==0:
+            even.append(lis[i])
+        else:
+            odd.append(lis[i])
+    even.sort(reverse=True)
+    odd.sort()
+    lis.clear()
+    lis.extend(odd)
+    lis.extend(even)
+    return lis
