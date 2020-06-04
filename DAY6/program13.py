fd9f26f53d2542a61ac3c318b53ca9df1e6c7813
Raveet kumar <66063741+ravi7799@users.noreply.github.com>
2020-06-04 22:40:12 +0530

    Update program13.py

--- DAY6/program13.py
+++ DAY6/program13.py
@@ -4,19 +4,20 @@ print("plz enter real numbers in this array:\n example- 0.3")
 for i in range(n):
     arr.append(input("enter real numbers greater than  zero:"))
 #use this for example
-#arr=['0.6', '0.2', '1', '0.5', '0.999', '0.88']
+#arr=['0.6', '0.2', '1', '2', '0.999', '88']
 
 print(arr)
 arr2=list(arr)
+arr3=[]
 for a in arr2:
-    print("a=",a)
     arr2.remove(a)
     for b in arr2:
         arr2.remove(b)
-        print("b=",b)
         for c in arr2:
-            print("a+b+c",a+b+c)
             if float(float(a)+float(b)+float(c))<2 and float(float(a)+float(b)+float(c))>1:
-                print("triplet found a=",a,"b=",b,"c=",c,"\na+b+c=",float(a)+float(b)+float(c),"\n")
+                if float(a)+float(b)+float(c) not in arr3:
+                    print("triplet found a=",a,"b=",b,"c=",c,"\na+b+c=",float(a)+float(b)+float(c),"\n")
+                    arr3.append(float(a)+float(b)+float(c))
         arr2.append(b)
     arr2.append(a)
+
